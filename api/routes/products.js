b5a929c847f19b215ff75ffa05d8683ee972c8f3
Aleksandr <aleksandrbartov@yandex.ru>
2018-04-26 04:03:25 +0400

    just installed mongoose and sucsessfully connected to DB

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,6 +5,12 @@ router.get('/', (req, res, next) => {
     res.status(200).json({
         message: 'we have get some products'
     })
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'we have post to /products'
+    })
 })
 
 module.exports = router;
